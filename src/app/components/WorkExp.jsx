750961d80d7e7a762947307a2b11aaaa3bb3243c
fangedShadow <bhavpreet666singh.com>
2025-06-25 14:47:30 -0400

    update the portfolio

--- src/app/components/WorkExp.jsx
+++ src/app/components/WorkExp.jsx
@@ -21,57 +21,55 @@ function WorkExperience() {
                 </div>
                 <div className='flex justify-around p-2 gap-4 flex-col ml-4'>
                     <div>
-                        <a href="https://shelves-o7hn82av3-ayman-haques-projects.vercel.app" target="_blank" rel="noopener noreferrer">
-                            <div className='project_tile flex justify-center h-full sm:hover:rounded-lg sm:hover:bg-hoverBlack sm:hover:cursor-pointer sm:hover:animate-enlarge-text'>
-                                <div className='flex flex-col pb-2 pl-1 items-center hovText hover:bg-hoverBlack hover:cursor-pointer hover:rounded-lg sm:hover:none sm:hover:animate-none hover:animate-enlarge-text'>
-                                    <h1 className='sm:hidden text-lg text-greyWhite font-bold '>
-                                        One Community Global
-                                    </h1>
-                                    <div className='img flex sm:items-end items-center pt-4 sm:flex-row flex-col'>
-                                        <h3 className='text-lg text-greyWhite font-bold'>Jan 2025 - Present</h3>
-                                        <span className='sm:hidden text-small text-darkGreyWhite text-justify'>
-                                            At One Community Global, I contribute to the Highest Good Network project,
-                                            and its other related platform, where I help in development of new features, fixed bugs,
-                                            and participate in detailed pull request reviews using React.js, MongoDB,
-                                            Express, and Node.js. I built a listing overview feature to improve data
-                                            presentation and user experience, and designed an interactive map with geolocation
-                                            integration for better navigation and engagement.
-                                        </span>
-                                    </div>
-                                    <div className='flex-row flex-wrap justify-evenly gap-3 sm:hidden flex'>
-                                        <div className='bg-mainRed rounded-xl py-0.5 px-2 text-center'> React </div>
-                                        <div className='bg-mainRed rounded-xl py-0.5 px-2 text-center'> Node </div>
-                                        <div className='bg-mainRed rounded-xl py-0.5 px-2 text-center'> MongoDB </div>
-                                        <div className='bg-mainRed rounded-xl py-0.5 px-2 text-center'> Express </div>
-                                    </div>
-                                </div>
-                                <div className='w-9/12 hidden sm:flex sm:items-start sm:flex-col pl-4 pt-4 gap-1 hovText'>
-                                    <h1 className='xl:text-xl text-lg text-greyWhite font-bold'>
-                                        One Community Global
-                                    </h1>
-                                    <p className='xl:text-base text-sm text-darkGreyWhite text-justify pr-2'>
+                        <div className='project_tile flex justify-center h-full sm:hover:rounded-lg sm:hover:bg-hoverBlack sm:hover:cursor-pointer sm:hover:animate-enlarge-text'>
+                            <div className='flex flex-col pb-2 pl-1 items-center hovText hover:bg-hoverBlack hover:cursor-pointer hover:rounded-lg sm:hover:none sm:hover:animate-none hover:animate-enlarge-text'>
+                                <h1 className='sm:hidden text-lg text-greyWhite font-bold '>
+                                    One Community Global
+                                </h1>
+                                <div className='img flex sm:items-end items-center pt-4 sm:flex-row flex-col'>
+                                    <h3 className='text-lg text-greyWhite font-bold'>Jan 2025 - Present</h3>
+                                    <span className='sm:hidden text-small text-darkGreyWhite text-justify'>
                                         At One Community Global, I contribute to the Highest Good Network project,
                                         and its other related platform, where I help in development of new features, fixed bugs,
                                         and participate in detailed pull request reviews using React.js, MongoDB,
                                         Express, and Node.js. I built a listing overview feature to improve data
                                         presentation and user experience, and designed an interactive map with geolocation
                                         integration for better navigation and engagement.
-                                        <span className='hidden lg:inline ml-1'>
-                                            I also developed the backend for a Wishlist feature, ensuring smooth data flow and
-                                            intuitive user interaction. In addition to building features, I resolved critical
-                                            bugs and hotfix issues to enhance overall system stability and performance, while
-                                            maintaining high code quality through consistent PR reviews and adherence to best practices.
-                                        </span>
-                                    </p>
-                                    <div className='flex-row flex-wrap justify-evenly gap-3 hidden sm:flex'>
-                                        <div className='bg-mainRed rounded-xl py-0.5 px-2 text-center'> React </div>
-                                        <div className='bg-mainRed rounded-xl py-0.5 px-2 text-center'> Node </div>
-                                        <div className='bg-mainRed rounded-xl py-0.5 px-2 text-center'> MongoDB </div>
-                                        <div className='bg-mainRed rounded-xl py-0.5 px-2 text-center'> Express </div>
-                                    </div>
+                                    </span>
+                                </div>
+                                <div className='flex-row flex-wrap justify-evenly gap-3 sm:hidden flex'>
+                                    <div className='bg-mainRed rounded-xl py-0.5 px-2 text-center'> React </div>
+                                    <div className='bg-mainRed rounded-xl py-0.5 px-2 text-center'> Node </div>
+                                    <div className='bg-mainRed rounded-xl py-0.5 px-2 text-center'> MongoDB </div>
+                                    <div className='bg-mainRed rounded-xl py-0.5 px-2 text-center'> Express </div>
                                 </div>
                             </div>
-                        </a>
+                            <div className='w-9/12 hidden sm:flex sm:items-start sm:flex-col pl-4 pt-4 gap-1 hovText'>
+                                <h1 className='xl:text-xl text-lg text-greyWhite font-bold'>
+                                    One Community Global
+                                </h1>
+                                <p className='xl:text-base text-sm text-darkGreyWhite text-justify pr-2'>
+                                    At One Community Global, I contribute to the Highest Good Network project,
+                                    and its other related platform, where I help in development of new features, fixed bugs,
+                                    and participate in detailed pull request reviews using React.js, MongoDB,
+                                    Express, and Node.js. I built a listing overview feature to improve data
+                                    presentation and user experience, and designed an interactive map with geolocation
+                                    integration for better navigation and engagement.
+                                    <span className='hidden lg:inline ml-1'>
+                                        I also developed the backend for a Wishlist feature, ensuring smooth data flow and
+                                        intuitive user interaction. In addition to building features, I resolved critical
+                                        bugs and hotfix issues to enhance overall system stability and performance, while
+                                        maintaining high code quality through consistent PR reviews and adherence to best practices.
+                                    </span>
+                                </p>
+                                <div className='flex-row flex-wrap justify-evenly gap-3 hidden sm:flex'>
+                                    <div className='bg-mainRed rounded-xl py-0.5 px-2 text-center'> React </div>
+                                    <div className='bg-mainRed rounded-xl py-0.5 px-2 text-center'> Node </div>
+                                    <div className='bg-mainRed rounded-xl py-0.5 px-2 text-center'> MongoDB </div>
+                                    <div className='bg-mainRed rounded-xl py-0.5 px-2 text-center'> Express </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
